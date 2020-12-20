89cb4dce980573d262d47b35958bddb520f7a58f
Taen Ahammed <taenahammed7@gmail.com>
2020-12-20 08:39:14 +0600

    Install helment and enable some middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const startupDebugger = require("debug")("app:startup");
 const express = require("express");
+const helmet = require("helmet");
 
 const app = express();
 const home = require("./routes/home");
@@ -7,6 +8,9 @@ const home = require("./routes/home");
 app.set("view engine", "pug");
 
 app.use("/", home);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => startupDebugger(`Listening on port ${port}...`));
